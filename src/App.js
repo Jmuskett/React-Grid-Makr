63f384fd7f1012a2db4dce167c4b446594c1eb7e
Jmuskett <hi@johnmuskett.com>
2019-05-05 20:00:35 +0100

    add basic maths logic to grid creator

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React from "react";
 import Grid from "./Components/Grid";
+import PageHeader from "./Components/PageHeader";
+import PrimaryContent from "./Components/PrimaryContent";
+export const GridContext = React.createContext();
 
 function App() {
   return (
-    <Grid rows={3} columns={4}>
-      <div>test</div>
-      <div>test</div>
-      <div>test</div>
-      <div>test</div>
-      <div>test</div>
+    <Grid rows={4} columns={2}>
+      <PageHeader>This is the header</PageHeader>
+
+      <PrimaryContent>
+        <div>
+          <p>Hello</p>
+        </div>
+        <div>
+          <p>Hello</p>
+        </div>{" "}
+        <div>
+          <p>Hello</p>
+        </div>
+      </PrimaryContent>
+      <PrimaryContent>
+        <div>
+          <p>Hello</p>
+        </div>{" "}
+        <div>
+          <p>Hello</p>
+        </div>{" "}
+        <div>
+          <p>Hello</p>
+        </div>{" "}
+        <div>
+          <p>Hello</p>
+        </div>
+      </PrimaryContent>
     </Grid>
   );
 }
--- src/Components/Grid.js
+++ src/Components/Grid.js
@@ -1,23 +1,28 @@
 import React from "react";
 import styled from "@emotion/styled";
+import { GridContext } from "../App";
 
 const GridContainer = styled.div`
   display: grid;
-  grid-template-columns: ${p => `repeat(${p.columns}, auto)`};
+  grid-template-columns: ${p =>
+    `repeat(${p.columns}, ${100 / p.numberOfElements}%)`};
   grid-template-rows: ${p => `repeat(${p.rows}, auto)`};
+  grid-column-gap: 2rem;
 `;
 
 const Grid = props => {
   const { rows, columns } = props;
-  const GridContext = React.createContext({ rows, columns });
 
+  const numberOfElements = props.children.length - 1;
   return (
-    <GridContext.Provider>
-      <GridContainer columns={columns} rows={rows}>
+    <GridContext.Provider value={{ rows, columns, numberOfElements }}>
+      <GridContainer
+        columns={columns}
+        rows={rows}
+        numberOfElements={numberOfElements}>
         {props.children}
       </GridContainer>
     </GridContext.Provider>
   );
 };
-
 export default Grid;
